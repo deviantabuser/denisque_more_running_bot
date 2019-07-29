c64665c762ad110f69df26526b43a563e7ee7509
deviantabuser <37623267+deviantabuser@users.noreply.github.com>
2019-07-29 12:20:27 +0500

    Add files via upload

---
+++ 123.js
@@ -0,0 +1,6 @@
+const Telegraf = require('telegraf')
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply('Welcome!'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
